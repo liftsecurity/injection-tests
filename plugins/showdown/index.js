d38b6ca60a8dbbc69135977924cd1e08f9d0c314
Adam Baldwin <baldwin@andyet.net>
2014-01-05 01:05:25 -0800

    added showdown support

---
+++ plugins/showdown/index.js
@@ -0,0 +1,45 @@
+var Hapi = null; // Initialized during plugin registration
+var Showdown = require('showdown');
+var converter = new Showdown.converter();
+
+exports.name = "showdown";
+exports.version = "1.0.0";
+
+var internals = {};
+
+internals.defaults = {
+    handler: function (request, config, next) {
+        next();
+    }
+};
+
+exports.register = function (plugin, options, next) {
+    internals.setHapi(plugin.hapi);
+    var Utils = plugin.hapi.utils
+    var settings = Utils.applyToDefaults(internals.defaults, options);
+
+    plugin.views({
+        engines: { jade: 'jade' },
+        path: __dirname + '/templates'
+    });
+ 
+    plugin.route({ method: 'GET', path: '/' + exports.name, handler: function (request, reply) {
+        reply.view("plugin", {marked: "", sanitized: "", name: exports.name});
+    }});
+
+    plugin.route({
+        method: 'POST',
+        path: '/' + exports.name,
+        handler: function (request, reply) {
+            reply.view("plugin", {input: request.payload.input, sanitized: converter.makeHtml(request.payload.input), name: exports.name});
+    }});
+
+    next();
+};
+
+
+internals.setHapi = function (module) {
+    Hapi = Hapi || module;
+};
+
+
