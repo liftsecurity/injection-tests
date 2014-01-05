10e059ff26b7526ca79279b293e9edf667688a45
Adam Baldwin <baldwin@andyet.net>
2014-01-05 01:18:10 -0800

    added support for markdown-js

---
+++ plugins/markdown-js/index.js
@@ -0,0 +1,44 @@
+var Hapi = null; // Initialized during plugin registration
+var markdown_js = require('markdown-js');
+
+exports.name = "markdown-js";
+exports.version = "1.0.0";
+
+var internals = {};
+
+internals.defaults = {
+    handler: function (request, config, next) {
+        next();
+    }
+};
+
+exports.register = function (plugin, options, next) {
+    internals.setHapi(plugin.hapi);
+    var Utils = plugin.hapi.utils
+    var settings = Utils.applyToDefaults(internals.defaults, options);
+
+    plugin.views({
+        engines: { jade: 'jade' },
+        path: __dirname + '/templates'
+    });
+ 
+    plugin.route({ method: 'GET', path: '/' + exports.name, handler: function (request, reply) {
+        reply.view("plugin", {marked: "", sanitized: "", name: exports.name});
+    }});
+
+    plugin.route({
+        method: 'POST',
+        path: '/' + exports.name,
+        handler: function (request, reply) {
+            reply.view("plugin", {input: request.payload.input, sanitized: markdown_js.makeHtml(request.payload.input), name: exports.name});
+    }});
+
+    next();
+};
+
+
+internals.setHapi = function (module) {
+    Hapi = Hapi || module;
+};
+
+
